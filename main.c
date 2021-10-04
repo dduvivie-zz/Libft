e002c3164c48703ee1cf0653552d27b8cd607066
Fulou-Shotoku <75534111+Fulou-Shotoku@users.noreply.github.com>
2021-10-04 15:07:38 +0200

    5 functions done

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "libft.h"
+
+int	main(void)
+{
+	printf("%d", isalnum('f'));
+	printf("%d\n", ft_isalnum('f'));
+	printf("%d", isalpha('{'));
+	printf("%d\n", ft_isalpha('{'));
+	printf("%d", isdigit('9'));
+	printf("%d\n", ft_isdigit('9'));
+	printf("%d", isascii(0x80));
+	printf("%d\n", ft_isascii(0x80));
+	printf("%d", isprint(126));
+	printf("%d\n", ft_isprint(126));
+	return (0);
+}
