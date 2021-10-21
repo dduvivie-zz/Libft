9d5d02171ae3d028cc462700e3f3d5f8897988de
dduvivie <danaeduvivier@gmail.com>
2021-10-21 16:00:16 +0200

    wip

---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 15:25:06 by dduvivie          #+#    #+#             */
+/*   Updated: 2021/10/21 15:59:00 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	len;
+
+	len = ft_strlen(src);
+	while (--dstsize && *src)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+	}
+	*dst = '\0';
+	return (len);
+}
