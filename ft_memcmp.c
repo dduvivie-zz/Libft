7c056e9fd98332810ba91b12a492283c836aa326
dduvivie <danaeduvivier@gmail.com>
2021-10-25 15:39:12 +0200

    wip

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 15:09:32 by dduvivie          #+#    #+#             */
+/*   Updated: 2021/10/25 15:37:14 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*str1;
+	char	*str2;
+
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while (--n && (unsigned char)*str1 == (unsigned char)*str2)
+	{
+		str1++;
+		str2++;
+	}
+	if ((unsigned char)*str1 > (unsigned char)*str2)
+		return (1);
+	else if ((unsigned char)*str1 == (unsigned char)*str2)
+		return (0);
+	else
+		return (-1);
+}
