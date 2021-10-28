eb1ddd93fba85b9798d9fb9cd2624101ad1b61c4
dduvivie <danaeduvivier@gmail.com>
2021-10-28 10:53:26 +0200

    check remain

---
+++ ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 10:39:20 by dduvivie          #+#    #+#             */
+/*   Updated: 2021/10/28 10:52:02 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = (ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (0);
+	while (*s1)
+	{
+		str[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2)
+	{
+		str[i] = *s2;
+		s2++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_substr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 10:25:33 by dduvivie          #+#    #+#             */
+/*   Updated: 2021/10/28 10:38:10 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	int		i;
+
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (0);
+	while (s[start] != '\0')
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*
+int	main(void)
+{
+	char	*s = "Hello";
+	char	*str = ft_substr(s, 1, 4);
+
+	printf("%s\n", str);
+	free(str);
+	return (0);
+}
+*/
