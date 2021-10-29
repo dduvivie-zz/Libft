a1843907a8f4e75cd060c9e95c9476100d516861
dduvivie <danaeduvivier@gmail.com>
2021-10-29 15:06:01 +0200

    check remain

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 13:13:55 by dduvivie          #+#    #+#             */
-/*   Updated: 2021/10/28 13:13:56 by dduvivie         ###   ########.fr       */
+/*   Updated: 2021/10/29 15:05:24 by dduvivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count_words(char const *s, char c)
+static int	count_words(char const *s, char c)
 {
 	int	count;
 	int	i;
@@ -40,26 +40,103 @@ int	count_words(char const *s, char c)
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static char	*get_str(const char *s, char c)
 {
-	int		words;
-	char	**t;
+	int		i;
+	int		len;
+	char	*str;
 
-	words = count_words(s, c);
-	t = malloc((words + 1) * sizeof(char *));
-	if (!t)
+	i = 0;
+	len = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
 		return (0);
-	// get the length of each words to do malloc
-	return(0);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
+static void	split_free(char **t, int elem)
+{
+	int	i;
+
+	i = 0;
+	while (i < elem)
+	{
+		free(t[i]);
+		i++;
+	}
+	free(t);
+}
+
+static char	*get_first_char(char const *s, char c, int flag)
+{
+	if (flag)
+	{
+		while (*s == c)
+			s++;
+	}
+	else
+	{
+		while (*s != c && *s)
+			s++;
+	}
+	return ((char *)s);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	char	**t;
+
+	t = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!t)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		s = get_first_char(s, c, 1);
+		if (*s)
+		{
+			t[i] = get_str(s, c);
+			if (!t[i])
+			{
+				split_free(t, i);
+				return (0);
+			}
+			s = get_first_char(s, c, 0);
+			i++;
+		}
+	}
+	t[i] = 0;
+	return (t);
+}
+
+/*
 int	main(void)
 {
-	char const	*s = "Blue Black White Red";
-	char		c = ' ';
+	char const	*s = "*Blue*Black*Pink*Yellow*Green*White*Red*";
+	char		c = '*';
 	char		**t = ft_split(s, c);
+	int			i;
 
 	if (!t)
 		exit(0);
+	while (t[i])
+	{
+		printf("%s\n", t[i]);
+		i++;
+	}
 	return (0);
 }
+*/
