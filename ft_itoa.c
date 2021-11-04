49e8d7f197ec7cd0d9a1c5ecf56ae345434f89fa
dduvivie <danaeduvivier@gmail.com>
2021-11-04 15:03:08 +0100

    check remain

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,24 +6,57 @@
 /*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 15:07:15 by dduvivie          #+#    #+#             */
-/*   Updated: 2021/10/29 15:07:16 by dduvivie         ###   ########.fr       */
+/*   Updated: 2021/11/04 15:02:49 by dduvivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int	get_len(int n)
 {
-	int	num;
+	int		count;
+	double	num;
 
-
-	return (0);
+	count = 0;
+	num = (double)n;
+	if (num < 0)
+	{
+		num *= -1;
+		count++;
+	}
+	while (num > 9)
+	{
+		count++;
+		num = num / 10;
+	}
+	count++;
+	return (count);
 }
 
-int	main(void)
+char	*ft_itoa(int n)
 {
-	printf("%d\n", 42 / 10);
+	int				len;
+	char			*str;
+	long long int	num;
 
-	printf("%s\n", ft_itoa(42));
-	return (0);
+	len = get_len(n);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	str[len] = '\0';
+	num = (long long int)n;
+	if (num < 0)
+	{
+		num *= -1;
+		str[0] = '-';
+	}
+	len--;
+	while (len > 0 && num > 9)
+	{
+		str[len] = (num % 10) + '0';
+		num = num / 10;
+		len--;
+	}
+	str[len] = (num % 10) + '0';
+	return (str);
 }
