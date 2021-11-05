4f8c8394a547fffc3bd0cf8543f3983103eebff0
dduvivie <danaeduvivier@gmail.com>
2021-11-05 15:42:21 +0100

    to remove

--- ftest.c
+++ ftest.c
@@ -1,30 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
-{
-	int		diff;
-	char	*str1;
-	char	*str2;
 
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	while (--n && (unsigned char)*str1 == (unsigned char)*str2)
-	{
-		str1++;
-		str2++;
-	}
-	if ((unsigned char)*str1 > (unsigned char)*str2)
-		return (1);
-	else if ((unsigned char)*str1 == (unsigned char)*str2)
-		return (0);
-	else
-		return (-1);
-}
 
 int	main(void)
 {
-	printf("%d\n", memcmp("abcde", "abcds", 5));
-	printf("%d\n", ft_memcmp("abcde", "abcds", 5));
+	char *s = "1234567";
+
+	printf("%c\n", *s);
+	s += 2;
+	printf("%\n", *s);
 	return (0);
 }
