291bef52c87b79bf1fa75030917ac004f518a866
dduvivie <danaeduvivier@gmail.com>
2021-11-08 16:25:22 +0100

    done

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,31 +11,45 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static char	*str_cpy(char *dst, char *src, int reverse, size_t len)
+{
+	while (len--)
+	{
+		*dst = *src;
+		if (reverse)
+		{
+			src--;
+			dst--;
+		}
+		else
+		{
+			src++;
+			dst++;
+		}
+	}
+	return (dst);
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*src_last;
-	char	*dst_last;
+	char	*src_i;
+	char	*dst_i;
+	int		reverse;
 
 	if ((char *)dst < (char *)src)
 	{
-		while (len--)
-		{
-			*(char *)dst = *(char *)src;
-			dst++;
-			src++;
-		}
+		src_i = (char *)src;
+		dst_i = (char *)dst;
+		reverse = 0;
 	}
 	else
 	{
-		src_last = (char *)src + ((int)len - 1);
-		dst_last = (char *)dst + ((int)len - 1);
-		while (len--)
-		{
-			*dst_last = *src_last;
-			src_last--;
-			dst_last--;
-		}	
+		src_i = (char *)src + ((int)len - 1);
+		dst_i = (char *)dst + ((int)len - 1);
+		reverse = 1;
 	}
+	str_cpy(dst_i, src_i, reverse, len);
 	return (dst);
 }
