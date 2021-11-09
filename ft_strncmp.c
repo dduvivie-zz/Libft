4fd5c525f091af6b39d71dd58fa40151ab7cd081
dduvivie <danaeduvivier@gmail.com>
2021-11-09 14:24:36 +0100

    done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,9 +12,17 @@
 
 #include "libft.h"
 
+/*
+This function compares the two strings s1 and s2. 
+It returns an integer less than, equal to, or greater than zero 
+if s1 (or the first n bytes thereof) is found, respectively, 
+to be less than, to match, or be greater than s2.
+*/
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (--n && (unsigned int)*s1 == (unsigned int)*s2)
+	if (n == 0)
+		return (0);
+	while (--n > 0 && (unsigned int)*s1 == (unsigned int)*s2)
 	{
 		s1++;
 		s2++;
