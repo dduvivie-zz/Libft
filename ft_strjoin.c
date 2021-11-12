9d4eff818140d9663a314e119743967ea1aac74f
dduvivie <danaeduvivier@gmail.com>
2021-11-12 11:42:27 +0100

    complete

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,12 +6,19 @@
 /*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 10:39:20 by dduvivie          #+#    #+#             */
-/*   Updated: 2021/10/28 10:52:02 by dduvivie         ###   ########.fr       */
+/*   Updated: 2021/11/12 11:42:02 by dduvivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+This function allocates (with malloc) and returns a new string,
+which is the result of the concatenation of ’s1’ and ’s2’.
 
+Parameters: 1. The prefix string.
+			2. The suffix string.
+Return:		The new string. NULL if the allocation fails.
+*/
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
